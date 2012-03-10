f2a7b0353887b109d9d63e71752ba9b04ec1df99
friflaj <friflaj@redminebacklogs.net>
2012-03-10 16:06:58 +0100

    Site update

--- update.sh
+++ update.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-rm -f ~/redmine/www.redminebacklogs.net/coverage/*
-cp -r ~/redmine/redmine/coverage/* ~/redmine/www.redminebacklogs.net/coverage/
-cd ~/redmine/www.redminebacklogs.net
 git add .
 git commit -a -m "Site update"
 git push
